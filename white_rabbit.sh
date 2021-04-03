fd64259ae2abc7c7c3bbd662e051a350a0eb34dc
Thales Carl <tclavoratti@gmail.com>
2021-04-02 21:29:05 -0300

    Add explanation for set -eu command

--- white_rabbit.sh
+++ white_rabbit.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
+set -eu
+#set -e: abort script if a command returns with a non-zero exit code
+#set -u: abort script if a variable name is wrong
 
 clear
 echo "Wake up, Neo!"
-
 
 #This is a one line comment
 
