5a124cd56bffb73c5da97e26f2aca71eea37089c
Thales Carl <tclavoratti@gmail.com>
2021-04-02 21:36:48 -0300

    Add pipe scripting recipe

---
+++ pipes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Pipes mean: Send output from one script to another script
+
+MESSAGE="Wake up, Neo!"
+export MESSAGE
+./pipes2.sh
---
+++ pipes2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Trinity said: $MESSAGE"
